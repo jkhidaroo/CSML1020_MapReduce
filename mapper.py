41df878eb2dfc0eba89f8a3fc812d3c62ac8d577
jkhidaroo <jkhidaroo@rssdimed.com>
2020-06-12 12:00:18 -0400

    Changed separator to ':'

--- mapper.py
+++ mapper.py
@@ -8,7 +8,7 @@ def read_input(file):
         # split the line into words
         yield line.split()
 
-def main(separator='\t'):
+def main(separator=':'):
     # input comes from STDIN (standard input)
     data = read_input(sys.stdin)
     for words in data:
@@ -18,7 +18,7 @@ def main(separator='\t'):
         #
         # tab-delimited; the trivial word count is 1
         for word in words:
-            print '%s%s%d' % (word, separator, 1)
+            print (word, separator, 1)
 
 if __name__ == "__main__":
     main()
--- reducer.py
+++ reducer.py
@@ -5,11 +5,11 @@ from itertools import groupby
 from operator import itemgetter
 import sys
 
-def read_mapper_output(file, separator='\t'):
+def read_mapper_output(file, separator=':'):
     for line in file:
         yield line.rstrip().split(separator, 1)
 
-def main(separator='\t'):
+def main(separator=':'):
     # input comes from STDIN (standard input)
     data = read_mapper_output(sys.stdin, separator=separator)
     # groupby groups multiple word-count pairs by word,
@@ -19,7 +19,7 @@ def main(separator='\t'):
     for current_word, group in groupby(data, itemgetter(0)):
         try:
             total_count = sum(int(count) for current_word, count in group)
-            print "%s%s%d" % (current_word, separator, total_count)
+            print (current_word, separator, total_count)
         except ValueError:
             # count was not a number, so silently discard this item
             pass
